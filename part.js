d4adb953219edcee9aec5c356ad8b3fe3f69c6ed
VISHWAJBANDI <126488543+VISHWAJBANDI@users.noreply.github.com>
2024-11-09 11:05:43 +0530

    Add files via upload

---
+++ part.js
@@ -0,0 +1,24 @@
+
+let str = "Hello World!";
+alert(str.replace("World","My Dear Friend"))
+
+// o/p:Hello My Dear Friend!
+
+let a="Hungry";
+alert(a.repeat(10))
+// it will repeat hungry 10 times
+
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+alert(sentence.lastIndexOf("fox"))
+
+// 49
+
+let message = "Hello, world!";
+let startsWithHi = message.startsWith("hi");
+console.log(startsWithHi); // Output: false
+
+
+let fileName = "document.pdf";
+let endsWithPdf = fileName.endsWith(".pdf");
+console.log(endsWithPdf); // Output: true
+
